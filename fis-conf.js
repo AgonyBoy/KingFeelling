b3b80e63eb505f2915bcd14eac4215d5485883e9
olddriver <13265364432@163.com>
2017-07-03 19:30:08 +0800

    sdfsdf

--- fis-conf.js
+++ fis-conf.js
@@ -1,9 +1,43 @@
 
 /*框架发布配置*/
 
-fis.match("/components/*",{
-	release:"/components/$0"
-});
+		fis.match("/components/**",{
+			release:"/assets/$0"
+		});
 
-/*server端静态目录配置*/
-fis.set("statics","/assets");
+		fis.match("/static/**",{
+			release:"/assets/$0"
+	});
+
+/*js，css，png，jpg，gif等文件签名*/
+
+		fis.match('/static/**.{js,css,png,jpg,gif}', {
+		  useHash: true
+		});
+
+/*js,css,png,jpg,gif等文件压缩*/
+
+		fis.match('/static/js/*.js', {
+		  optimizer: fis.plugin("uglify-js"),
+		  release: '/assets/$0'
+		});
+
+		fis.match('/static/css/*.css', {
+		  // fis-optimizer-clean-css 插件进行压缩，已内置
+		  optimizer: fis.plugin('clean-css')
+		});
+
+		fis.match('/static/img/*.{jpg,gif,png}', {
+		  // fis-optimizer-png-compressor 插件进行压缩，已内置
+		  optimizer: fis.plugin('png-compressor')
+		});
+
+fis.match({
+	deploy:[
+		fis.plugin('release',{
+			from:'/static/css/',
+			to:'../../assets/static/css'
+		}),
+		fis.plugin('local-deliver')
+	]
+});
--- static/js/public.js
+++ static/js/public.js
@@ -6,3 +6,14 @@
 		// _node.addClass("mui-active").siblings().removeClass("mui-active");
 		mui(".mui-slider").slider().gotoItem(index);
 	});
+
+	//添加回到顶部导航按钮
+	$(window).scroll(function(){
+		var _top =$(this).scrollTop();
+		var _wd =$(this).width();
+		if(_top > 1000 && _wd > 480){
+			if($("a.nextop").length==0){$("body").append("<a href='#' class='nextop' title='回到顶部'><i class='kfl top0'></i></a>");}
+		}else{
+			$("a.nextop").remove();  //删除瞄点顶部按钮
+		}
+	});
